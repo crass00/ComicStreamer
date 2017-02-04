493a110b449a09744b8e42c11171b68889674738
Tristan Crispijn <Tristan@MacBook-Pro.local>
2017-02-04 17:09:03 +0100

    ..
    trying to fix publisher...

--- comicstreamerlib/server.py
+++ comicstreamerlib/server.py
@@ -230,14 +230,14 @@ class JSONResultAPIHandler(GenericAPIHandler):
         query = addQueryOnList(query, Comic.alternateseries_raw, AlternateSeries.name, alternateseries)
         query = addQueryOnList(query, Comic.genres_raw, Genre.name, genre)
         
-        if hasValue(series_filter):
-            query = query.filter( Comic.series.ilike(unicode(series_filter).replace("*","%") ))
-        if hasValue(title_filter):
-            query = query.filter( Comic.title.ilike(unicode(title_filter).replace("*","%") ))
+        #if hasValue(series_filter):
+        #    query = query.filter( Comic.series.ilike(unicode(series_filter).replace("*","%") ))
+        #if hasValue(title_filter):
+        #    query = query.filter( Comic.title.ilike(unicode(title_filter).replace("*","%") ))
         #if hasValue(filename_filter):
         #    query = query.filter( Comic.path.ilike(unicode(filename_filter).replace("*","%") ))
-        if hasValue(publisher):
-            query = query.filter( Comic.publisher.ilike(unicode(publisher).replace("*","%") ))
+        #if hasValue(publisher):
+        #    query = query.filter( Comic.publisher.ilike(unicode(publisher).replace("*","%") ))
         #if hasValue(character):
         #    query = query.filter( Comic.characters_raw.any(Character.name.ilike(unicode(character).replace("*","%") )))
         #if hasValue(tag):
