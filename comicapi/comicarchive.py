fe55bc69cca6d36efa318d7d9e43a7c3aef22ee7
crass00 <crass00@hotmail.com>
2017-09-09 23:35:45 +0100

    Switch to rarfile .. part 2

--- comicapi/comicarchive.py
+++ comicapi/comicarchive.py
@@ -46,42 +46,6 @@ ImageExtensions = [".bmp", ".jpg", "jpeg", ".png", ".gif", "webp", ".pcx", "tiff
 
 from comicstreamerlib.folders import AppFolders
 
-class OpenableRarFile(rarfile.RarFile):
-    def open(self, member):
-        #print "opening %s..." % member
-        # based on https://github.com/matiasb/python-unrar/pull/4/files
-        res = []
-        if isinstance(member, rarfile.RarInfo):
-            member = member.filename
-        archive = unrarlib.RAROpenArchiveDataEx(self.filename, mode=constants.RAR_OM_EXTRACT)
-        handle = self._open(archive)
-        found, buf = False, []
-        def _callback(msg, UserData, P1, P2):
-            if msg == constants.UCM_PROCESSDATA:
-                data = (ctypes.c_char*P2).from_address(P1).raw
-                buf.append(data)
-            return 1
-        c_callback = unrarlib.UNRARCALLBACK(_callback)
-        unrarlib.RARSetCallback(handle, c_callback, 1)
-        try:
-            rarinfo = self._read_header(handle)
-            while rarinfo is not None:
-                #print "checking rar archive %s against %s" % (rarinfo.filename, member)
-                if rarinfo.filename == member:
-                    self._process_current(handle, constants.RAR_TEST)
-                    found = True
-                else:
-                    self._process_current(handle, constants.RAR_SKIP)
-                rarinfo = self._read_header(handle)
-        except unrarlib.UnrarException:
-            raise rarfile.BadRarFile("Bad RAR archive data.")
-        finally:
-            self._close(handle)
-        if not found:
-            raise KeyError('There is no item named %r in the archive' % member)
-        return ''.join(buf)
-
-
 if platform.system() == "Windows":
     import _subprocess
 import time
@@ -94,8 +58,6 @@ except ImportError:
     pil_available = False
 
 sys.path.insert(0, os.path.abspath(".") )
-#import UnRAR2
-#from UnRAR2.rar_exceptions import *
 
 #from settings import ComicTaggerSettings
 from comicinfoxml import ComicInfoXml
@@ -502,11 +464,7 @@ class RarArchiver:
         while tries < 7:
             try:
                 tries = tries+1
-                #tmp_folder = tempfile.mkdtemp()
-                #tmp_file = os.path.join(tmp_folder, archive_file)
-                #rarc.extract(archive_file, tmp_folder)
-                data = rarc.open(archive_file)
-                #data = open(tmp_file).read()
+                data = rarc.open(archive_file).read()
                 entries = [(rarc.getinfo(archive_file), data)]
 
 
@@ -648,8 +606,7 @@ class RarArchiver:
         while tries < 7:
             try:
                 tries = tries+1
-                #rarc = UnRAR2.RarFile( self.path )
-                rarc = OpenableRarFile(self.path)
+                rarc = rarfile.RarFile( self.path )
 
             except (OSError, IOError) as e:
                 print >> sys.stderr, u"getRARObj(): [{0}] {1} attempt#{2}".format(str(e), self.path, tries)
